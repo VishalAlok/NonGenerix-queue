64e20439044abc96251ec7fec38efbde0bd38224
vishal.Alok <you@example.com>
2021-10-15 05:54:13 +0530

    Add project files.

---
+++ Queue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+
+namespace NonGenericQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue Obj = new Queue();
+            Obj.Enqueue("Hello1");
+            Obj.Enqueue("Hello2");
+            Obj.Enqueue("Hello3");
+ 
+            foreach (string s in Obj)
+            {
+                Console.WriteLine(s + " ");
+            }
+            Obj.Dequeue();
+            foreach (string s in Obj)
+            {
+                Console.WriteLine(s + " ");
+            }
+            Console.WriteLine(Convert.ToString(Obj.Peek()));
+        }
+    }
+}
